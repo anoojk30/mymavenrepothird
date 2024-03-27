7dcecba1e717f89d1257c1a65a7914218efbb9a0
ūśēr̥ <r@DESKTOP-KUEKH95>
2024-03-27 21:03:52 +0530

    thirdproject

---
+++ javanew/src/main/java/javanew/Multiplication.java
@@ -0,0 +1,14 @@
+package javanew;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=10;
+		int mult=a*b;
+		System.out.println(mult);
+
+	}
+
+}
